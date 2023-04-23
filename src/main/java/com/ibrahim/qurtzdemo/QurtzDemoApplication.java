dee84a1dc5b0cbda977a7660322e5bef4635235b
İbrahim BAYRAMLI <ibrahim.bayramli@atmosware.turkcell.com.tr>
2023-04-23 18:05:26 +0300

    proje oluşturuldu

---
+++ src/main/java/com/ibrahim/qurtzdemo/QurtzDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ibrahim.qurtzdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QurtzDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QurtzDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ibrahim/qurtzdemo/QurtzDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibrahim.qurtzdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QurtzDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
